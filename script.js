8c5d66dc8140274af69ef777b2b1e3943827a337
andrew-erasmus <andrew.r.eras@gmail.com>
2023-07-09 22:05:40 +0200

    Add functionality to update images on click

--- script.js
+++ script.js
@@ -23,43 +23,69 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "rock") {
         playWinCount++;
         compWinCount++;
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "paper") { 
+        updateImages(playerSelection, computerSelection);
+        //getImage and update score
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "paper") {
         playWinCount++;
         compWinCount++;
+        updateImages(playerSelection, computerSelection);
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "scissors") {
         playWinCount++;
         compWinCount++;
+        updateImages(playerSelection, computerSelection);
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
         playWinCount++;
+        updateImages(playerSelection, computerSelection);
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
         playWinCount++;
+        updateImages(playerSelection, computerSelection);
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
         playWinCount++;
+        updateImages(playerSelection, computerSelection);
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") {
         compWinCount++;
+        updateImages(playerSelection, computerSelection);
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock") {
         compWinCount++;
+        updateImages(playerSelection, computerSelection);
     } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors") {
         compWinCount++;
+        updateImages(playerSelection, computerSelection);
     }
 }
 
+function updateImages(playerSelection, computerSelection) {
+    const player = document.querySelector('#playerImg');
+    player.src = getImage(playerSelection.toLowerCase());
+    const computer = document.querySelector('#compImg');
+    computer.src = getImage(computerSelection.toLowerCase());
+}
 
-
-function game(playerSelection) {
-
-    for (let i = 0; i < 5; i++) {
-        const rock = document.querySelector('#rock');
-        const paper = document.querySelector('#paper');
-        const scissors = document.querySelector('#scissors');
-        const computerSelection = getComputerChoice();
-
-        rock.addEventListener('click', playRound('rock', computerSelection));
-        paper.addEventListener('click', playRound('paper', computerSelection));
-        scissors.addEventListener('click', playRound('scissors', computerSelection));
-        // add event listen for buttons for users selection
-        //UPDATE SCORE WITH RESULT
+function getImage(option) {
+    //returns the src for the imgs
+    if (option === 'rock') {
+        return './images/rock.png';
+    } else if (option === 'paper') {
+        return "./images/paper.png";
+    } else if (option === 'scissors') {
+        return "./images/scissors.png";
     }
+}
+
+function updateResult() {
+    //counts the score
+}
+
+function game(playerSelection, computerSelection) {
+
+
+
+    playRound(playerSelection,computerSelection);
+
+    // add event listen for buttons for users selection
+
+    //UPDATE SCORE WITH RESULT
+
     if (playWinCount === compWinCount) {
         console.log("It's a tie.");
     } else if (playWinCount > compWinCount) {
@@ -71,5 +97,15 @@ function game(playerSelection) {
 
 let playWinCount = 0;
 let compWinCount = 0;
-game();
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const computerSelection = getComputerChoice();
+
+rock.addEventListener('click', game('rock', computerSelection));
+paper.addEventListener('click', game('paper', computerSelection));
+scissors.addEventListener('click', game('scissors', computerSelection));
+
+// FOR SOME REASON CLICKS WITHOUT CLICKING -- CHECK EVENT LISTENERS
+
 
