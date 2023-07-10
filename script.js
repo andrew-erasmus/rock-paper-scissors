0df556c560197d87a060f535d697cc9408d23d93
andrew-erasmus <andrew.r.eras@gmail.com>
2023-07-10 03:10:43 +0200

    Add modal overlay for end of game screen

--- script.js
+++ script.js
@@ -91,22 +91,72 @@ function getImage(option) {
 function updateResult() {
     //counts the score
     const score = document.querySelector('.score-sheet');
-    console.log(score)
     score.textContent = `${playWinCount} : ${compWinCount}`
+
+
+
+    const overlay = document.getElementById('overlay');
+    const endText = document.querySelector('.end-text');
+
+    if (playWinCount === 5 && compWinCount === 5) {
+        const modal = document.querySelector('#modal');
+        endText.textContent="It's a tie!!";
+        openModal(modal);
+
+    } else if (playWinCount === 5) {
+        const modal = document.querySelector('#modal');
+        openModal(modal);
+        endText.textContent="Congratulations! You Won!";
+        // player wins
+        
+    } else if (compWinCount === 5) {
+        const modal = document.querySelector('#modal');
+        openModal(modal);
+        endText.textContent="Unlucky! You lose!";
+        // computer wins
+    }
+
+    function openModal(modal) {
+        if (modal == null) return
+        modal.classList.add('active');
+        overlay.classList.add('active');
+        endText.classList.add('active');
+    }
+
+    function closeModal(modal) {
+        if (modal == null) return
+        modal.classList.remove('active');
+        overlay.classList.remove('active');
+        endText.classList.add('active');
+    }
+    const closeModalButton = document.querySelector('.close-button');
+
+    closeModalButton.addEventListener('click', () => {
+        closeModal(modal);
+        location.reload();
+    })
+
+    overlay.addEventListener('click', () => {
+        const modals = document.querySelectorAll('.modal.active');
+        closeModal(modal);
+        location.reload();
+    })
+
 }
 
 
-let playWinCount = 0;
-let compWinCount = 0;
+let scores = document.querySelector('.score-sheet');
+let scoresString = scores.textContent.split(":");
+let playWinCount = scoresString[0].trim();
+let compWinCount = scoresString[1].trim();
 
 const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
 
 
-rock.addEventListener('click', e => {playRound('rock', getComputerChoice()) });
-paper.addEventListener('click', e => {playRound('paper', getComputerChoice())});
-scissors.addEventListener('click', e => {playRound('scissors', getComputerChoice())});
-
+rock.addEventListener('click', e => { playRound('rock', getComputerChoice()) });
+paper.addEventListener('click', e => { playRound('paper', getComputerChoice()) });
+scissors.addEventListener('click', e => { playRound('scissors', getComputerChoice()) });
 
 
